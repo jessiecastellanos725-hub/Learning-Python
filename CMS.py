0bf38a9bc17d9530795f8e95c8c4fd58bea86d7a
Jessie Castellanos <jessiecastellanos725@gmail.com>
2025-12-21 15:45:31 -0500

    This is the first commit.

---
+++ CMS.py
@@ -0,0 +1,37 @@
+# Dictonary that will hold all of the contacts
+user_contacts = {
+    'Jessie' : {'Phone Number': '999-999-9999', 'email': 'jessie@emailprovider.com'}
+}
+
+# TODO: Create an add function. Need to confirm that the information is not the exact same.
+
+# TODO: Create a view function. What should the funcion recieve? What shout it return? Wha should it not do?
+
+# TODO: Create a search function. What should the funcion recieve? What shout it return? Wha should it not do?
+
+# TODO: Create an edit function. What should the funcion recieve? What shout it return? Wha should it not do?
+
+# TODO: Create a delete function. What should the funcion recieve? What shout it return? Wha should it not do?
+
+# TODO: Create the CLI menu. It needs to be able to loop back to the menu. Can't crash on bad input. The user can exit cleanly.
+
+"""
+1. Show menu
+2. Add a contact
+3. View all contacts
+4. Search contacts
+5. Edit contact
+6. Delete contact
+
+After each step:
+
+* Run the program
+* Try to break it intentionally
+"""
+
+# TODO: Add Input validation & Error Handling.
+
+# TODO: Add File Peristence.
+
+# TODO: Refactor the code.
+
