b3389d4791ac26370af6fdcb6e4a417e001ffdb4
jessiecastellanos725-hub <jessiecastellanos725@gmail.com>
2025-12-31 14:14:32 -0500

    Application for vulnerable flask app.
    Goal is to create the Flask app and then secure it later.

---
+++ Vulnerable_Flask.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from datetime import datetime
+import sqlite3
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
